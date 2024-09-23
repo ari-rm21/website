7babbfabd7507c1a1089bc6e510b06dcf216cca3
Ariana Rodriguez <94588385+ari-rm21@users.noreply.github.com>
2024-09-22 19:35:01 -0600

    website name updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,7 +41,7 @@ export class AppComponent implements OnInit {
       }
     });
   }
-  title = 'ARM PORTFOLIO';
+  title = 'ARM WEBSITE';
   menuOpen: boolean = false;
 
   toggleMenu(event: Event) {
